412c69f3c234e2f6087885a766cb697e84266cb9
Zhenia <example@gmail.com>
2022-07-01 15:33:20 +0300

    created first javascipr programm

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+
+const   a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("Во сколько его оцените?", ""),
+        c = prompt("Один из последних просмотренных фильмов?", ""),
+        d = prompt("Во сколько его оцените?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
